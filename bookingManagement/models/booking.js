b3aa7f264a36cb935e86d15fbf7aae0ec0b12b44
SangIT465 <transang040605@gmail.com>
2025-03-03 10:19:50 +0700

    booking_system

---
+++ bookingManagement/models/booking.js
@@ -0,0 +1,6 @@
+let bookings = [
+    { id: 1, customerName: 'Trần Văn Sang', date: '2025-02-22', time: '11:00', status: 'Pending' },
+    { id: 2, customerName: 'Hoàng Văn Huy', date: '2025-02-23', time: '15:00', status: 'Confirmed' }
+];
+
+module.exports = bookings;
